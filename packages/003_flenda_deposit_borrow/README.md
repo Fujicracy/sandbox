3460818fb71780e4598660506e8bd87846b94e0a
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-07-19 18:22:31 -0400

    added basic experiment for flenda deposit and borrow

---
+++ packages/003_flenda_deposit_borrow/README.md
@@ -0,0 +1,8 @@
+## Experiment 003_flenda_deposit_borrow
+
+
+This experiment showcases a user who wants to deposit-and-borrow crosschain in the following way:
+1. A cross call is originated from Chain A, using Connext Amarokv2.0 to relay message.
+2. When message is received on Chain B, a simplified Fuji instance makes a deposit-borrow operation on chain B.
+3. When funds are received fuji instances sends funds back to Chain A. 
+
---
+++ packages/003_flenda_deposit_borrow/contracts/Vault.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+
+contract Vault is ERC4626{
+
+  constructor(address asset) ERC4626(IERC20Metadata(asset)) ERC20('shares Of Asset', 'shA') {
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal pure override {
+    from;
+    to;
+    amount;
+  }
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal pure override {
+    from;
+    to;
+    amount;
+  }
+
+}
