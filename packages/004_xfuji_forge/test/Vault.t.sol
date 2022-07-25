324b376200b61bb80b843bd38833581bdb5a524d
Boyan Barakov <boyan.barakov@gmail.com>
2022-07-25 11:44:26 +0200

    init tests

--- packages/004_xfuji_forge/test/Vault.t.sol
+++ packages/004_xfuji_forge/test/Vault.t.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-contract ContractTest is Test {
+contract VaultTest is Test {
     function setUp() public {}
 
-    function testExample() public {
+    function testDeposit() public {
         assertTrue(true);
     }
 }
