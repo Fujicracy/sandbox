7317eb7fb66f58d001d241a4fc111cfdc3aa0f13
Boyan Barakov <boyan.barakov@gmail.com>
2022-07-25 12:38:15 +0200

    add rpcs in foundry.toml

--- packages/004_xfuji_forge/test/Vault.t.sol
+++ packages/004_xfuji_forge/test/Vault.t.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 contract VaultTest is Test {
-    function setUp() public {}
+  function setUp() public {}
 
-    function testDeposit() public {
-        assertTrue(true);
-    }
+  function testDeposit() public {
+      assertTrue(true);
+  }
 }
