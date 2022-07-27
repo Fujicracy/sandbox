6eb1028546a6d88484b212f96047897f7bb2ceb5
Boyan Barakov <boyan.barakov@gmail.com>
2022-07-27 12:19:22 +0200

    add: Deployer contract

---
+++ packages/004_xfuji_forge/src/DeployerTestnet.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.9;
+
+import {IConnextHandler} from "nxtp/core/connext/interfaces/IConnextHandler.sol";
+import {Vault} from "./Vault.sol";
+import {Router, IWETH9} from "./Router.sol";
+import {AaveV3Goerli} from "./providers/goerli/AaveV3Goerli.sol";
+import {AaveV3Rinkeby} from "./providers/rinkeby/AaveV3Rinkeby.sol";
+import {ILendingProvider} from "./interfaces/ILendingProvider.sol";
+
+contract DeployerTestnet {
+
+  struct Registry {
+    address asset;
+    address debtAsset;
+    address oracle;
+    address weth;
+    address testToken;
+    address connextHandler;
+  }
+
+  struct Deploys {
+    Vault vault;
+    Router router;
+    ILendingProvider provider;
+  }
+
+  Vault.Factor maxLtv = Vault.Factor(75, 100);
+  Vault.Factor liqRatio = Vault.Factor(5, 100);
+
+  mapping(string => Registry) public registryByChain;
+  mapping(string => Deploys) public deploysByChain;
+
+  constructor() {
+    Registry memory goerli = Registry({
+      asset: 0x2e3A2fb8473316A02b8A297B982498E661E1f6f5,
+      debtAsset: 0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43,
+      oracle: 0xD7E3AE6f48A1D442069b32a5Aa6e315B111B992C,
+      weth: 0x2e3A2fb8473316A02b8A297B982498E661E1f6f5,
+      testToken: 0x26FE8a8f86511d678d031a022E48FfF41c6a3e3b,
+      connextHandler: 0x6c9a905Ab3f4495E2b47f5cA131ab71281E0546e
+    });
+    registryByChain["goerli"] = goerli;
+
+    Registry memory rinkeby = Registry({
+      asset: 0xd74047010D77c5901df5b0f9ca518aED56C85e8D,
+      debtAsset: 0xb18d016cDD2d9439A19f15633005A6b2cd6Aa774,
+      oracle: 0xD7E3AE6f48A1D442069b32a5Aa6e315B111B992C,
+      weth: 0xd74047010D77c5901df5b0f9ca518aED56C85e8D,
+      testToken: 0x3FFc03F05D1869f493c7dbf913E636C6280e0ff9,
+      connextHandler: 0x4cAA6358a3d9d1906B5DABDE60A626AAfD80186F
+    });
+    registryByChain["rinkeby"] = rinkeby;
+  }
+
+  function deploy(string calldata chainName) public {
+    Registry memory reg = registryByChain[chainName]; 
+    if (reg.asset == address(0)) {
+      revert("No registry for this chain");
+    }
+
+    ILendingProvider aaveV3;
+    if (keccak256(bytes(chainName)) == keccak256(bytes("goerli"))) {
+      aaveV3 = new AaveV3Goerli();
+    } else {
+      aaveV3 = new AaveV3Rinkeby();
+    }
+    Router router = new Router(
+      IWETH9(reg.weth),
+      IConnextHandler(reg.connextHandler)
+    );
+    Vault vault = new Vault(
+      reg.asset,
+      reg.debtAsset,
+      reg.oracle,
+      address(router),
+      maxLtv,
+      liqRatio
+    );
+
+    deploysByChain[chainName].vault = vault;
+    deploysByChain[chainName].router = router;
+    deploysByChain[chainName].provider = aaveV3;
+  }
+}
