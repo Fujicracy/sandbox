0dcebcbc755a17dfb55f4226b59cdaf72754f273
Boyan Barakov <boyan.barakov@gmail.com>
2022-08-07 18:27:44 +0200

    add factory and chief

---
+++ packages/004_xfuji_forge/src/vaults/borrowing/BorrowingVaultFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+import "./BorrowingVault.sol";
+import "../../abstracts/VaultDeployer.sol";
+
+/// @notice Contract for deploying Trident exchange Hybrid Vault with configurations.
+/// @author Mudit Gupta.
+contract BorrowingVaultFactory is VaultDeployer {
+  constructor(address _chief) VaultDeployer(_chief) {}
+
+  function deployVault(bytes memory _deployData) external returns (address vault) {
+    (address asset, address debtAsset, address oracle) = abi.decode(_deployData, (address, address, address));
+
+    // @dev Salt is not actually needed since `_deployData` is part of creationCode and already contains the salt.
+    bytes32 salt = keccak256(_deployData);
+    vault = address(new BorrowingVault{salt: salt}(asset, debtAsset, oracle, chief));
+    _registerVault(vault, asset, salt);
+  }
+}
+
