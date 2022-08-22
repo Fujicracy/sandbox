b00ffcb79a905a6289c480cdd239fee1c5084057
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-08-22 12:03:21 +0200

    refacto: experiment1

---
+++ packages/007_frontend-notifications/pages/components/experiment1.tsx
@@ -0,0 +1,40 @@
+import { useActor, useSelector } from "@xstate/react"
+import { NextComponentType } from "next"
+import { useContext } from "react"
+import { GlobalStateContext } from "../_app"
+import Button from "./Button"
+import { AuthStateMachine } from "./machines/authMachine"
+
+const selectUser = (machine: AuthStateMachine) => machine.context.user
+const selectError = (machine: AuthStateMachine) => machine.context.error
+
+const Experiment1: NextComponentType = () => {
+  const { authService } = useContext(GlobalStateContext)
+  const [state, send] = useActor(authService)
+  const error = useSelector(authService, selectError)
+  const user = useSelector(authService, selectUser)
+
+  return (
+    <div className="border rounded border-slate-300 px-4 py-2">
+      <p>
+        <strong>Experiment #1:</strong> Login with blocknative & xstate
+      </p>
+      <p>
+        Current state of authService <code>{state.value}</code>
+      </p>
+      {state.context.user ? (
+        <Button onClick={() => send("LOGOUT")}>Log out</Button>
+      ) : (
+        <Button onClick={() => send("INITIALIZE")}>Login</Button>
+      )}
+      {state.context.error && <p>Error: {error}</p>}
+      {state.context.user && (
+        <p>
+          User: <code>{user.address}</code>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Experiment1
--- packages/007_frontend-notifications/pages/index.tsx
+++ packages/007_frontend-notifications/pages/index.tsx
@@ -1,21 +1,9 @@
-import { useActor, useSelector } from "@xstate/react"
 import type { NextPage } from "next"
 import Head from "next/head"
-import { useContext } from "react"
-import Button from "./components/Button"
+import Experiment1 from "./components/experiment1"
 import Experiment2 from "./components/experiment2"
-import { AuthStateMachine } from "./machines/authMachine"
-import { GlobalStateContext } from "./_app"
-
-const selectUser = (machine: AuthStateMachine) => machine.context.user
-const selectError = (machine: AuthStateMachine) => machine.context.error
 
 const Home: NextPage = () => {
-  const { authService } = useContext(GlobalStateContext)
-  const [state, send] = useActor(authService)
-  const error = useSelector(authService, selectError)
-  const user = useSelector(authService, selectUser)
-
   return (
     <div className="container mx-auto">
       <Head>
@@ -27,26 +15,7 @@ const Home: NextPage = () => {
       <main>
         <h1 className="text-2xl py-8">Welcome to yome's playground 🎯👾</h1>
 
-        <div className="border rounded border-slate-300 px-4 py-2">
-          <p>
-            <strong>Experiment #1:</strong> Login with blocknative & xstate
-          </p>
-          <p>
-            Current state of authService <code>{state.value}</code>
-          </p>
-          {state.context.user ? (
-            <Button onClick={() => send("LOGOUT")}>Log out</Button>
-          ) : (
-            <Button onClick={() => send("INITIALIZE")}>Login</Button>
-          )}
-          {state.context.error && <p>Error: {error}</p>}
-          {state.context.user && (
-            <p>
-              User: <code>{user.address}</code>
-            </p>
-          )}
-        </div>
-
+        <Experiment1 />
         <Experiment2 />
       </main>
     </div>
