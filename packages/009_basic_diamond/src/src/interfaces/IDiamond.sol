1b4a4cbfa5768714b3d5bcb6e7e7adc5a8896b3c
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-05-25 12:46:14 +0200

    feat: defining basic diamond

---
+++ packages/009_basic_diamond/src/src/interfaces/IDiamond.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.17;
+
+/**
+ * @author Nick Mudge (https://twitter.com/mudgen)
+ * @notice EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
+ */
+
+interface IDiamond {
+    enum FacetCutAction {
+        Add,
+        Replace,
+        Remove
+    }
+    // Add=0, Replace=1, Remove=2
+
+    struct FacetCut {
+        address facetAddress;
+        FacetCutAction action;
+        bytes4[] functionSelectors;
+    }
+
+    event DiamondCut(FacetCut[] _diamondCut, address _init, bytes _calldata);
+}
