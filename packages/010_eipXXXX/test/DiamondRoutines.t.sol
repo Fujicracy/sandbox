e5c8e63509d7d0881f3e0dd10a865c7c5668c268
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-06-21 01:16:10 +0100

    feat(010): added function to deploy diamond yield vault

--- packages/010_eipXXXX/test/DiamondRoutines.t.sol
+++ packages/010_eipXXXX/test/DiamondRoutines.t.sol
@@ -8,8 +8,11 @@ import {OwnershipFacet} from "../src/facets/diamond/OwnershipFacet.sol";
 import {IDiamond} from "../src/interfaces/IDiamond.sol";
 import {DiamondArgs, Diamond} from "../src/Diamond.sol";
 import {IERC20} from "../src/interfaces/IERC20.sol";
+import {VaultConstants} from "./utils/VaultConstants.t.sol";
+import {VaultAssets} from "../src/facets/vault/VaultAssets.sol";
+import {VaultYieldInit} from "../src/upgradeInitializers/VaultInit.sol";
 
-contract DiamondRoutines is DiamondConstants {
+contract DiamondRoutines is DiamondConstants, VaultConstants {
   function _deployDiamondEssentials()
     private
     returns (IDiamond.FacetCut memory, IDiamond.FacetCut memory)
@@ -33,15 +36,27 @@ contract DiamondRoutines is DiamondConstants {
     diamond = address(new Diamond(coreFacets, args));
   }
 
-  // function _deployDiamondYieldVault(
-  //   address asset,
-  //   address chief,
-  //   string memory name,
-  //   string memory symbol
-  // )
-  //   internal
-  // {
-  //   IDiamond.FacetCut[] memory yieldVaultFacets = new IDiamond.FacetCut[](3);
-  //   (yieldVaultFacets[0], yieldVaultFacets[1]) = _deployDiamondEssentials();
-  // }
+  function _deployDiamondYieldVault(
+    address asset,
+    address chief,
+    string memory name,
+    string memory symbol
+  )
+    internal
+    returns (address)
+  {
+    IDiamond.FacetCut[] memory yieldVaultFacets = new IDiamond.FacetCut[](3);
+    (yieldVaultFacets[0], yieldVaultFacets[1]) = _deployDiamondEssentials();
+
+    VaultAssets vault = new VaultAssets();
+    yieldVaultFacets[3] =
+      IDiamond.FacetCut(address(vault), IDiamond.FacetCutAction.Add, erc4626Selectors);
+    init = address(new VaultYieldInit());
+
+    bytes memory initArgs =
+      abi.encodeWithSelector(VaultYieldInit.init.selector, asset, chief, name, symbol);
+    DiamondArgs memory args = DiamondArgs(address(this), init, initArgs);
+    diamond = address(new Diamond(yieldVaultFacets, args));
+    return address(diamond);
+  }
 }
